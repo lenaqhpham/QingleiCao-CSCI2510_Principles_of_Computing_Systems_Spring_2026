aa4438a65547b89a6c2bd0db8cbcb22f91997cf0
lenaqhpam <lenaxpham5@gmail.com>
2026-02-26 21:13:07 -0600

    submit studio 2

--- studio-2-input-output/copy.c
+++ studio-2-input-output/copy.c
@@ -1,1 +1,26 @@
-//Replace this file with your copy.c
+
+#include <unistd.h>
+#define bufferSize 200
+
+int main() {
+	char buffer[bufferSize];
+	ssize_t bytesRead;
+	while (1) {
+		bytesRead = read(STDIN_FILENO, buffer, bufferSize);
+		if (bytesRead == 0) {
+			break;
+		}
+		if (bytesRead < 0) {
+			return 1; // error
+		}
+		ssize_t bytesWritten = 0;
+		while (bytesWritten < bytesRead) {
+			ssize_t result = write(STDOUT_FILENO, buffer + bytesWritten, bytesRead - bytesWritten);
+			if (result < 0) {
+				return 1; // error
+			}
+			bytesWritten += result;
+		}
+	}
+	return 0;
+}
